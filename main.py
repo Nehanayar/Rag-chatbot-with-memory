7cdf6b5514fb515fbd5d13966c495a6a7468da2b
Nehanayar <nehanayar27@gmail.com>
2026-07-02 18:44:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,4 @@
-# from langchain_google_genai import GoogleGenerativeAIEmbeddings
-# from langchain_community.document_loaders import PyPDFLoader
-# from langchain_community.vectorstores import FAISS
-# from langchain_text_splitters import RecursiveCharacterTextSplitter
-# from dotenv import load_dotenv
-# import os
-#
-# load_dotenv()
-#
-# embeddings = GoogleGenerativeAIEmbeddings(
-#     model="models/embedding-001",
-#     google_api_key=os.getenv("GEMINI_API_KEY")
-# )
-#
-# # load data from file
-# loader = PyPDFLoader("sample.pdf")
-# docs = loader.load()
-#
-# # split into chunks
-# splitter = RecursiveCharacterTextSplitter(
-#     chunk_size=1000,
-#     chunk_overlap=200
-# )
-# chunks = splitter.split_documents(docs)
-#
-# # create vector db
-# db = FAISS.from_documents(
-#     documents=chunks,
-#     embedding=embeddings
-# )
-#
-# db.save_local("pdf_db")
-# print("FAISS database created successfully!")
+
 
 
 
